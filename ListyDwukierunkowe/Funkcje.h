16b7888a62bb3aadbd53782d7be02ca303ed2a34
Rafał Klinowski <klinowskirafal@gmail.com>
2020-02-27 14:19:12 +0100

    Rozwalono listy dwukierunkowe.

--- ListyDwukierunkowe/Funkcje.h
+++ ListyDwukierunkowe/Funkcje.h
@@ -3,6 +3,7 @@
 using namespace std;
 
 typedef int T;
+typedef int typ;
 
 struct element
 {
@@ -11,16 +12,36 @@ struct element
 	element* pNext;
 };
 
-// zrobione:
+// Plik Lista_dwukierunkowa.pdf
+
 void dodajNaPoczatek(element*& pHead, element*& pTail, const T& wartosc);
+
 void dodajNaKoniec(element*& pHead, element*& pTail, const T& wartosc);
+
 element* znajdz(element* pHead, const T& wartosc);
+
 void usunListe(element*& pHead, element*& pTail);
+
 void usunElementy(element*& pHead, element*& pTail, const T& wartosc);
 
-// skipowane narazie:
 void dodajDoListyPosortowanej(element*& pHead, element*& pTail, const T& wartosc);
+
 void odwrocListe(element*& pHead, element*& pTail);
-element* przenies(element*& pHeadWej, element*& pTailWej, const T& wartosc, element*& pHeadWyj, element*& pTailWyj);
+
+void przenies(element*& pHeadWej, element*& pTailWej, const T& wartosc, element*& pHeadWyj, element*& pTailWyj);
+
 void posortujListe(element*& pHead, element*& pTail);
+
 void przesunListe(element*& pHead, element*& pTail);
+
+// Wyklad
+
+void usunRekurencyjnie(element*& pHead, element*& pTail);
+
+void wypiszOdKonca(element* pTail);
+
+void wypiszOdPoczatku(element* pHead);
+
+// Powtorzone
+
+void dodajNaPoczatek(element*& pHead, element*& pTail, const typ& wartosc);
--- ListyDwukierunkowe/ListyDwukierunkowe.cpp
+++ ListyDwukierunkowe/ListyDwukierunkowe.cpp
@@ -2,5 +2,33 @@
 
 int main()
 {
+	element* pGlowa = new element;
+	element* pOgon = new element;
+	pGlowa->wartosc = 3;
+	pGlowa->pNext = pOgon;
+	pGlowa->pPrev = nullptr;
+	pOgon->wartosc = 4;
+	pOgon->pPrev = pGlowa;
+	pOgon->pNext = nullptr;
+
+	//element* pGlowa2 = nullptr;
+	//element* pOgon2 = nullptr;
+
+	for (int i = 10; i > 3; i--)
+	{
+		dodajNaKoniec(pGlowa, pOgon, i);
+	}
+
+	wypiszOdPoczatku(pGlowa); cout << endl;
+
+	//dodajDoListyPosortowanej(pGlowa, pOgon, -5);
+	//odwrocListe(pGlowa, pOgon);
+	//przenies(pGlowa, pOgon, 3, pGlowa2, pOgon2);
+	//przesunListe(pGlowa, pOgon);
+	posortujListe(pGlowa, pOgon);
+
+	wypiszOdPoczatku(pGlowa); cout << endl;
+	//wypiszOdPoczatku(pGlowa2); cout << endl;
+
 	return 0;
 }
